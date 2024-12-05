e8e514252aa345887e6dafeceb33c296e50e3d7f
Natali <ns@numbuster.com>
2024-12-05 12:20:14 +0300

    Initial Commit

---
+++ FireplaceSUI/FireplaceSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireplaceSUIApp.swift
+//  FireplaceSUI
+//
+//  Created by Natalia Sinitsyna on 05.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct FireplaceSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
